fdc1b1c9c21a52902aec8b275ca4bbe0ab4f810e
Clever <clever@example.com>
2025-11-02 10:50:02 +0800

    init backend

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "✅ API is running!" });
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
